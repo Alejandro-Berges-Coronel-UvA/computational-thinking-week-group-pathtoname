6836373a6619bca00c171ab0ccb0262451fa092a
TDMWIFNB <viggopauw@gmail.com>
2024-09-06 15:57:03 +0200

    Edited station 1 file

--- challenge_day2/station1.py
+++ challenge_day2/station1.py
@@ -1,10 +1,14 @@
-# The algorithmn is the Fibonacci sequence
+# The algorithmn is the Fibonacci sequenc
+def solution_station_1(input):
+    # Fibonacci sequence
+    n = int(input)
+    fib = [0, 1]
+    for i in range(2, n+1):
+        fib.append(fib[i-1] + fib[i-2])
+    return fib[n]
 
-def solution_station_1(input_1):
-    if input_1 <= 0:
-        return 0
-    if input_1 == 1:
-        return 1
-    else:
-        return solution_station_1(input_1-1) + solution_station_1(input_1-2)
+print(solution_station_1(47))
+
+
+
     
